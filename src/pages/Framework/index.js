afafc4d5e46d5966e8b9f8eed91d767ee32c3fa1
steelbrain <i@steelbrain.me>
2017-06-16 22:47:41 +0530

    Add Framework page

--- src/pages/Framework/index.js
+++ src/pages/Framework/index.js
@@ -6,83 +6,99 @@ import Button from '../../components/Button/Button'
 const FrameworkPage = (props) => {
   return (
     <Default {...props}>
-      <br />
-      <div className={styles.header}>
-        <h1 className='center' style={{ marginBottom: '20px' }}>What is the Serverless Framework?</h1>
-        <p className='center' style={{ marginBottom: '20px' }}>
-          Build applications comprised of microservices that run in response to events, auto-scale for you, and only charge you when they run. This lowers the total cost of maintaining your apps, enabling you to build more logic, faster.
-        </p>
-      </div>
-      <iframe
-        className='center' src='https://player.vimeo.com/video/186516527?color=FFD734&byline=0&portrait=0' width='640'
-        height='360'
-        frameBorder='0'
-        allowFullScreen='true'
-      />
-      <p className='center' style={{ marginTop: '20px', marginBottom: '10px' }}>
-        The Serverless Framework is the world’s leading development framework for building serverless architectures.
-      </p>
-      <div className={styles.button}>
-        <Button
-          className={styles.button}
-          kind='black'
-          href='/framework/docs/'
-        >
-          Get Started
-        </Button>
-      </div>
-      <h2 className={styles.header} style={{ marginBottom: '20px', marginTop: '50px' }}>
-        Why Use The Framework
-      </h2>
-      <div className={styles.componentBox}>
-        <div className={styles.component}>
-          <img role='presentation' src='https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/icons/flash.svg' />
-          <h3>Move Fast</h3>
-          <p>Provision and deploy a REST API, data pipe-line, or one of many other use cases in minutes.
-          </p>
+
+      <div className={styles.hero}>
+        <div className={styles.heroContent}>
+          <h1 className={styles.heroHeading}>Build more, manage less</h1>
+          <p className={styles.heroDescription}>The Serverless Framework is the world’s leading development framework for building serverless, event-driven applications.</p>
+          <Button className={styles.button} kind='red' href='/framework/docs/'>Get Started</Button>
         </div>
-        <div className={styles.component}>
-          <img role='presentation' src='https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/icons/signs.svg' />
-          <h3>Simplicity</h3>
-          <p>Our CLI makes it simple to manage and build a serverless architecture by abstracting away provider-level complexity.
-          </p>
-        </div>
-        <div className={styles.component}>
-          <img role='presentation' src='https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/icons/time-is-money.svg' />
-          <h3>100% Utilization</h3>
-          <p>Pay when your code runs, so you never have to worry about paying for idle server time.
-          </p>
+        <img className={styles.heroImage} src='/assets/images/icon-framework.png' />
+      </div>
+      <div className={styles.section}>
+        <h2 className={styles.featuresHeader}>Features</h2>
+        <div className={styles.featureBox}>
+          <div className={styles.feature}>
+            <img role='presentation' src='/assets/images/icon-extensible.svg' />
+            <h3>Extensible</h3>
+            <p>Build with a plugin architecture, the Serverless Frame is easily extensible with Servelss Plugins. Add custom functionality with a custom plugin or use one of the dozens of plugins the community has already created.</p>
+          </div>
+          <div className={styles.feature}>
+            <img role='presentation' src='/assets/images/icon-provider-agnostic.svg' />
+            <h3>Provider Agnostic</h3>
+            <p>Avoid vendor lockin and deploy apps on any cloud provider. Currently supported providers include AWS, Microsoft Azure, Google Cloud Platform, and IBM OpenWhisk.</p>
+          </div>
+          <div className={styles.feature}>
+            <img role='presentation' src='/assets/images/icon-opensource.svg' />
+            <h3>Open Source</h3>
+            <p>The Serverless Framework is an MIT open source project, actively maintained by a vibrant and engaged community of developers.</p>
+          </div>
+          <div className={styles.feature}>
+            <img role='presentation' src='/assets/images/icon-simple-devops.svg' />
+            <h3>Simple DevOps</h3>
+            <p>Build with a plugin architecture, the Serverless Frame is easily extensible with Servelss Plugins. Add custom functionality with a custom plugin or use one of the dozens of plugins the community has already created.</p>
+          </div>
+          <div className={styles.feature}>
+            <img role='presentation' src='/assets/images/icon-commandline.svg' />
+            <h3>Command Line</h3>
+            <p>Avoid vendor lockin and deploy apps on any cloud provider. Currently supported providers include AWS, Microsoft Azure, Google Cloud Platform, and IBM OpenWhisk.</p>
+          </div>
+          <div className={styles.feature}>
+            <img role='presentation' src='/assets/images/icon-multilanguage.svg' />
+            <h3>Multilanguage</h3>
+            <p>The Serverless Framework is an MIT open source project, actively maintained by a vibrant and engaged community of developers.</p>
+          </div>
         </div>
       </div>
-      <div className={styles.componentBox}>
-        <div className={styles.component}>
-          <img role='presentation' src='https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/icons/handshake.svg' />
-          <h3>Collaboration</h3>
-          <p>We provide a flexible application structure for easy management of code, resources, and events across large projects & teams.
-          </p>
-        </div>
-        <div className={styles.component}>
-          <img role='presentation' src='https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/icons/profile.svg' />
-          <h3>Community</h3>
-          <p>Serverless is an MIT open-source project, actively maintained by a vibrant and engaged community of developers.
-          </p>
-        </div>
-        <div className={styles.component}>
-          <img role='presentation' src='https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/icons/controls.svg' />
-          <h3>Infinite Scaleability</h3>
-          <p>Framework users are reacting to billions of events per month on AWS Lambda infrastructure.
-          </p>
+      <div className={styles.customers + " " + styles.section}>
+        <h2 className={styles.heading}>Trusted by</h2>
+        <div className={styles.customerLogos}>
+          <img className={styles.customerLogo} src='/assets/images/customer-nordstrom.png' />
+          <img className={styles.customerLogo} src='/assets/images/customer-coke.png' />
+          <img className={styles.customerLogo} src='/assets/images/customer-nike.png' />
         </div>
       </div>
-      <div className={styles.button}>
-        <Button kind='black' href='/framework/docs/'>
-        Get Started
-        </Button>
+      <div className={styles.section}>
+        <h2 className={styles.linksHeading}>Join the Serverless Community</h2>
+        <div className={styles.links}>
+          <a className={styles.link}>Serverless Newsletter</a>
+          <a className={styles.link}>Serverless Forum</a>
+          <a className={styles.link}>Serverless Gitter</a>
+          <a className={styles.link}>Github 16,540 Stars</a>
+        </div>
       </div>
-      <br />
-      <br />
-      <br />
-      <br />
+      <div className={styles.section}>
+        <h2 className={styles.casesHeading}>Use Cases</h2>
+        <div className={styles.cases}>
+          <div className={styles.case}>
+            <img role='presentation' src='/assets/images/icon-web-backend.svg' />
+            <h3>Web Backend</h3>
+            <p>Whether you’re building a public API or an internal backend for your app, the Serverless Framework is the easiest and most cost-effective way to build a production ready backend.</p>
+            <div className={styles.caseButtons}>
+              <Button kind='primary'>View Example</Button>
+              <Button kind='secondary'>Case Study</Button>
+            </div>
+          </div>
+          <div className={styles.case}>
+            <img role='presentation' src='/assets/images/icon-mobile-backend.svg' />
+            <h3>Mobile Backend</h3>
+            <p>The Serverless Framework is an easy and affordable way for mobile developers to spin-up an auto-scaling backend for their app. No maintenance or previous cloud experience required.</p>
+            <div className={styles.caseButtons}>
+              <Button kind='primary'>View Example</Button>
+              <Button kind='secondary'>Case Study</Button>
+            </div>
+          </div>
+          <div className={styles.case}>
+            <img role='presentation' src='/assets/images/icon-data-processing.svg' />
+            <h3>Data Processing</h3>
+            <p>The event-driven nature of the Serverless Framework makes it great for stream-based data processing. Continous scaling and subsecond metering mean it’s affordable and low-maintenence.</p>
+            <div className={styles.caseButtons}>
+              <Button kind='primary' className={styles.button}>View Example</Button>
+              <Button kind='secondary'>Case Study</Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </Default>
   )
 }
